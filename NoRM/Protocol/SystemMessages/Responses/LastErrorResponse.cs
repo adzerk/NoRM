f66ce0a1a34c551f07b47bfb718660573e00e4b8
averyj <javery@infozerk.com>
2010-05-09 18:24:50 +0000

    added UpdatedExisting to detect if an upsert inserts or updates

--- NoRM/Protocol/SystemMessages/Responses/LastErrorResponse.cs
+++ NoRM/Protocol/SystemMessages/Responses/LastErrorResponse.cs
@@ -16,6 +16,7 @@ namespace Norm.Responses
                                                        a.ForProperty(auth => auth.NumberOfErrors).UseAlias("n");
                                                        a.ForProperty(auth => auth.Error).UseAlias("err");
                                                        a.ForProperty(auth => auth.Code).UseAlias("code");
+                                                       a.ForProperty(auth => auth.UpdatedExisting).UseAlias("updatedExisting");
                                                    })
                 );
         }
@@ -25,15 +26,23 @@ namespace Norm.Responses
         /// </summary>
         /// <value>The number of errors.</value>
         public long? NumberOfErrors { get; set; }
+        
         /// <summary>
         /// Gets or sets the error.
         /// </summary>
         /// <value>The error.</value>
+        
         public string Error { get; set; }
         /// <summary>
         /// Gets or sets the code.
         /// </summary>
         /// <value>The code.</value>
+        
         public int Code { get; set; }
+
+        /// <summary>
+        /// When performing an upsert this value will be returned with a true if it updated an existing record and false if it inserted a new record.
+        /// </summary>
+        public bool UpdatedExisting { get; set; }
     }
 }
